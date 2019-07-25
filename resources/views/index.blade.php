bfb6b3746a79894a0d8b806e3c8d3390f03c9f49
Abdullah Aljufri <abdullahaljufri92@gmail.com>
2019-07-25 11:20:12 +0700

    Create index for view templating engine

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layout/main')
+@section('title','AdminLTE 2 | Dashboard')
+
+@extends('layout/header')
+@extends('layout/menu')
+@extends('layout/content')
+@extends('layout/footer')
+@extends('layout/setting')
